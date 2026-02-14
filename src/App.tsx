e7c90d498cc0dac547f67224401ba4d05b1bf15a
Marcin Ściborowski <9884143+msciborowski@users.noreply.github.com>
2026-02-14 19:44:22 +0100

    mui installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import { useTheme } from '@mui/material/styles'
+
 function App() {
+  const theme = useTheme()
   return (
-    <>
-      <div>Modena</div>
-    </>
+    <Box sx={{ p: 4, background: theme.palette.background.default, minHeight: '100vh' }}>
+      <Box sx={{ mb: 2, fontSize: 24, fontWeight: 'bold', color: theme.palette.primary.main }}>Modena</Box>
+      <Button variant="contained" color="primary" sx={{ borderRadius: 2 }}>
+        MUI Button
+      </Button>
+    </Box>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CacheProvider value={createCache({ key: 'css', prepend: true })}>
+      <ThemeProvider theme={createTheme()}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </CacheProvider>
   </StrictMode>,
 )
